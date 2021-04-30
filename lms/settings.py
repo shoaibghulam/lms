cd6f193a972168151dc280c3db402b417a840ae6
Mohammed Shoaib <shoaibbilal101@gmail.com>
2021-04-30 10:41:58 +0500

    Push COde

--- lms/settings.py
+++ lms/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
    
-    'lms.apps.SuitConfig',
+    # 'lms.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,16 +105,16 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 
 #Docker DB Configuration #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 #Docker DB Configuration #
 
@@ -122,17 +122,17 @@ DATABASES = {
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'NAME': 'LMS',
-#         'USER': 'postgres',
-#         'PASSWORD': 'shoaib',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5433'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'LMS',
+        'USER': 'postgres',
+        'PASSWORD': 'shoaib',
+        'HOST': '127.0.0.1',
+        'PORT': '5433'
+    }
+}
 
 #local Db Configuration
 

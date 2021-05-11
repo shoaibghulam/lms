00f7a2aed2334bbcd503f06f3f0eb17cb9a49d46
Mohammed Shoaib <shoaibbilal101@gmail.com>
2021-05-11 14:16:10 +0500

    Merge Shoaib Code

--- lms/settings.py
+++ lms/settings.py
@@ -126,11 +126,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'lms',
+        'NAME': 'LMS',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'shoaib',
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': '5433'
     }
 }
 
